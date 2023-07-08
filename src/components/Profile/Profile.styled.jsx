cc33b69032cda6ff1113594506eca59fa4b5426f
StasLiubinetskyi <121062371+StasLiubinetskyi@users.noreply.github.com>
2023-07-08 22:29:28 +0300

    test

---
+++ src/components/Profile/Profile.styled.jsx
@@ -0,0 +1,40 @@
+// const profileStyle = {
+//   maxWidth: '320px',
+//   margin: '0 auto',
+//   padding: '20px',
+//   border: '1px solid #ccc',
+//   borderRadius: '4px',
+//   textAlign: 'center',
+// };
+
+// const avatarStyle = {
+//   width: '100px',
+//   height: '100px',
+//   borderRadius: '50%',
+//   objectFit: 'cover',
+// };
+
+// const nameStyle = {
+//   marginTop: '10px',
+//   fontSize: '24px',
+//   fontWeight: 'bold',
+// };
+
+// const tagStyle = {
+//   marginTop: '5px',
+//   color: 'gray',
+// };
+
+// const locationStyle = {
+//   marginTop: '5px',
+//   color: 'gray',
+// };
+
+// const labelStyle = {
+//   fontWeight: 'bold',
+// };
+
+// const quantityStyle = {
+//   fontSize: '18px',
+//   color: '#555',
+// };
